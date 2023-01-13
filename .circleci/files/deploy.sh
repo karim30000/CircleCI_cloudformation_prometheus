5974149a9637c720412f6732c40ffbedd5895fe1
Karim Atef <katef749@gmail.com>
2023-01-13 18:28:58 +0200

    tryyy

--- .circleci/files/deploy.sh
+++ .circleci/files/deploy.sh
@@ -2,5 +2,6 @@
 
 aws cloudformation deploy \
          --template-file cloudfront.yml \
-         --stack-name InitialStack\
-         --parameter-overrides WorkflowID=063173971974
+         --stack-name InitialStack \
+         --parameter-overrides WorkflowID=063173971974 \
+         --tags project=063173971974
